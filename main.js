b1afe884effbc0ae91dd500611e4c8a13767f67e
TAndeweg <302275953@student.rocmondriaan.nl>
2022-01-20 16:41:24 +0100

    Update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//opdracht functions 
+//opdracht functions
 function temperatuur() {
     let temp = prompt('Hoe warm is het?');
 
@@ -17,42 +17,6 @@ function temperatuur() {
 
 temperatuur();
 
-
-
-
-let randomNumber = prompt('Kies een getal tussen de 1 en de 10.');
-
-function cijfers(randomNumber) {
-    if (randomNumber <= 5.5) {
-        document.getElementById('cijfer').innerHTML = 'Je hebt een onvoldoende gehaald.';
-    } else if (randomNumber >= 5.5 && randomNumber <= 10) {
-        document.getElementById('cijfer').innerHTML = 'Je hebt een voldoende gehaald.';
-    } else {
-        alert('Dit getal kan niet selecteer het opnieuw')
-    }
-}
-
-cijfers(randomNumber)
-
-
-
-
-
-
-
-let randomNumber = prompt('Kies een getal tussen de 1 en de 10.');
-
-function cijfers(randomNumber) {
-    if (randomNumber <= 5.5) {
-        document.getElementById('cijfer').innerHTML = 'Je hebt een onvoldoende gehaald.';
-    } else if (randomNumber >= 5.5 && randomNumber <= 10) {
-        document.getElementById('cijfer').innerHTML = 'Je hebt een voldoende gehaald.';
-    } else {
-        alert('Dit getal kan niet selecteer het opnieuw')
-    }
-}
-
-cijfers(randomNumber)
 
 
 
